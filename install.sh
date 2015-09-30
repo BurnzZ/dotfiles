e0234f40b658d0543f4ddaf3e1fc022d7e284fec
BurnzZ <kevinoxy@gmail.com>
2015-09-30 09:39:28 -0400

    added tmux configurationx

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-declare -a files=(aliases bash_profile bash_prompt exports functions gitconfig ackrc extra)
+declare -a files=(aliases bash_profile bash_prompt exports functions gitconfig ackrc extra tmux.conf)
 
 # loop through the files
 for file in ${files[@]}; do
