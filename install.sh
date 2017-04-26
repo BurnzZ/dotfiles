6d1e5caed44c356e430e39dcb80609ab6df749e7
BurnzZ <kevinoxy@gmail.com>
2017-04-26 12:02:28 +0800

    added zshrc

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-declare -a files=(aliases bash_profile bash_prompt exports functions gitconfig ackrc extra tmux.conf)
+declare -a files=(aliases bash_profile bash_prompt exports functions gitconfig ackrc extra tmux.conf zshrc)
 
 # loop through the files
 for file in ${files[@]}; do
