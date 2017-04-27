da1e2ed373af697bad0b1a4a9ff8a4032c99df09
BurnzZ <kevinoxy@gmail.com>
2017-04-27 23:55:45 +0800

    updated docs

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 declare -a files=(aliases bash_profile bash_prompt exports functions gitconfig ackrc extra tmux.conf zshrc)
 
-# loop through the files
+# loop through the files declared above and create a symlink of them in $HOME
 for file in ${files[@]}; do
     sourceFile="$(pwd)/$file"
     targetFile="$HOME/$(printf "%s" ".$file" | sed "s/.*\/\(.*\)/\1/g")"
